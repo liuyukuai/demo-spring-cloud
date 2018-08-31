9d21e299218f06529b777a6cd196cec606e0420f
liuyk <271007729@qq.com>
2018-08-31 13:32:18 +0800

     提交demo-spring-boot-parent

---
+++ demo-spring-boot-test-parent/src/main/java/com/itxiaoer/demo/DemoSpringBootTestParentApplication.java
@@ -0,0 +1,13 @@
+package com.itxiaoer.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBootTestParentApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(DemoSpringBootTestParentApplication.class, args);
+    }
+}
