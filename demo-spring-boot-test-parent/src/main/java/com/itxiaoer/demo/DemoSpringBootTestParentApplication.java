17479568d77859366a8e69b895719e88c6ad8bf6
liuyk <271007729@qq.com>
2018-08-31 13:35:20 +0800

    增加注释

--- demo-spring-boot-test-parent/src/main/java/com/itxiaoer/demo/DemoSpringBootTestParentApplication.java
+++ demo-spring-boot-test-parent/src/main/java/com/itxiaoer/demo/DemoSpringBootTestParentApplication.java
@@ -3,11 +3,13 @@ package com.itxiaoer.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author : liuyk
+ */
 @SpringBootApplication
 public class DemoSpringBootTestParentApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(DemoSpringBootTestParentApplication.class, args);
     }
 }
